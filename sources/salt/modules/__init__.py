58cc343757f74b869da90cb7f0ebb98da321cc4f
MichaelCho <zuow11@gmail.com>
2014-03-06 16:03:42 +0800

    [develop] fix '\r' bug in stdout

--- sources/salt/modules/__init__.py
+++ sources/salt/modules/__init__.py
@@ -5,4 +5,9 @@ Execution Module Directory
 
 def state_std(kwargs, res):
 	if kwargs and kwargs.has_key('state_ret'):
-		kwargs['state_ret']['state_stdout'] += res['stdout'] + '\n'
+		# remove \r from stdout
+		stdout_list = res['stdout'].split('\n')
+		for idx, item in enumerate(stdout_list):
+			stdout_list[idx] = item.split('\r')[-1]
+
+		kwargs['state_ret']['state_stdout'] += '\n'.join(stdout_list) + '\n'
