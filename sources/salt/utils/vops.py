8a87b903c12b0896ce0ea0e042236f3678ed7973
Thibault BRONCHAIN <thibault@mc2.io>
2014-07-16 17:06:50 +0800

    fix docker stream log

--- sources/salt/utils/vops.py
+++ sources/salt/utils/vops.py
@@ -2,38 +2,41 @@
 # @author: Thibault BRONCHAIN
 # (c) 2014 - MadeiraCloud
 
+# Imports
+import json
+
 # Defines
 PRINT_NOT=["stream"]
 
-def string_to_print(s, level, acc):
+def string_to_print(s, acc, level):
     if not s:
         return acc
     return acc+("  "*level)+s.encode('ascii','ignore').strip()+"\n"
 
-def list_to_print(l, level, acc):
+def list_to_print(l, acc, level):
     for item in l:
-        acc += obj_to_print(item,acc,level+1)
+        acc = obj_to_print(item,acc,level+1)
     return acc
 
-def dict_to_print(d, level, acc):
+def dict_to_print(d, acc, level):
     for key in d:
         if key not in PRINT_NOT:
-            acc += obj_to_print("%s:\n"%key,acc,level)
-        acc += obj_to_print(d[key],acc,level+1)
+            acc = obj_to_print("%s:\n"%key,acc,level)
+        acc = obj_to_print(d[key],acc,level+1)
     return acc
 
 def obj_to_print(o, acc, level=0):
     if type(o) is dict:
-        acc += dict_to_print(o,level,acc)
+        acc = dict_to_print(o,acc,level)
     elif type(o) is list:
-        acc += list_to_print(o,level,acc)
+        acc = list_to_print(o,acc,level)
     else:
-        acc += string_to_print(("%s"%o),level,acc)
+        acc = string_to_print(("%s"%o),acc,level)
     return acc
 
 def stream_to_print(s):
     ls = []
-    if s and s.startswith('{') and s.endswith('}'):
+    if s:# and s.startswith('{') and s.endswith('}'):
         delim = 0
         buf = ''
         for char in s:
